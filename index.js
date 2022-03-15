54ce14030236de69b47067aabc80d09c50377829
MSoulSol <masrimizo@gmail.com>
2022-03-14 21:56:00 -0400

    deleted unncessary commands that prevented javascript from running

--- index.js
+++ index.js
@@ -3,8 +3,7 @@ let header = document.createElement('h1')
 header.setAttribute('class','dog-name')
 header.append('Rizzo')
 
-let content = document.querySelector('.js-generated.content')
-let header=document.createElement ('h1')
-header.setAttribute('class', 'dog-name')
-header.append('Rizzo')
+
+
+
 content.append(header)
